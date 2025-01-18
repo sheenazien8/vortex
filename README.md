2db54d86dff1e995e0f18d993b29870959fbc820
sheenazien8 <sheenazien08@gmail.com>
2025-01-18 08:41:07 +0700

    add middleware and hook

---
+++ README.md
@@ -0,0 +1,105 @@
+# Vortex
+Simple HTTP and REST client library for Go, Inspired by Guzzle HTTP client from PHP.
+
+## Installation
+```sh
+go get github.com/sheenazien8/vortex
+```
+
+## Features
+- [x] Simple request
+- [x] POST, GET, DELETE, PUT, PATCH, DELETE
+- [x] Middleware
+
+
+## Usage
+```go
+package main
+
+import (
+	"github.com/sheenazien8/vortex"
+	"log"
+)
+
+func main() {
+	var response struct {
+		Data struct {
+			Email string `json:"email"`
+			Token string `json:"token"`
+		} `json:"data"`
+
+		Message string `json:"message"`
+		Status  bool   `json:"status"`
+	}
+
+	apiClient := vortex.New(vortex.Opt{
+		BaseURL: "https://lakasir.test",
+	})
+	resp, err := apiClient.
+		SetHeader("Content-Type", "application/json").
+		SetHeader("Accept", "application/json").
+		SetOutput(&response).
+		Post("/api/auth/login", map[string]interface{}{
+			"email":    "warunghikmah@lakasir.com",
+			"password": "password",
+		})
+	if err != nil {
+		panic(err)
+	}
+	if resp.StatusCode != 200 {
+		log.Fatal("status code is not 200 ", resp.StatusCode, string(resp.Body))
+	}
+
+	data := response.Data
+	var token = data.Token
+
+	meResponse, err := apiClient.
+		SetHeader("Content-Type", "application/json").
+		SetHeader("Accept", "application/json").
+		SetHeader("Authorization", "Bearer "+token).
+		SetOutput(&response).
+		Get("/api/auth/me")
+	if err != nil {
+		panic(err)
+	}
+
+	if meResponse.StatusCode != 200 {
+		log.Fatal("status code is not 200 ", meResponse.StatusCode, string(meResponse.Body))
+	}
+
+	println(response.Data.Email)
+}
+```
+
+## Middleware
+```go
+func LoggingMiddleware(req *http.Request, next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request: %s %s", r.Method, r.URL.String())
+		next.ServeHTTP(w, r)
+		log.Printf("Response: %s", w.Header().Get("StatusCode"))
+	}
+}
+
+apiClient := vortex.New(vortex.Opt{
+    BaseURL: "https://lakasir.test",
+})
+
+resp, err := apiClient.
+    UseMiddleware(LoggingMiddleware)
+```
+
+## Hook
+```go
+
+func ExampleHook(req *http.Request, resp *http.Response) {
+	log.Printf("Hook: Response status code: %d", resp.StatusCode)
+}
+
+apiClient := vortex.New(vortex.Opt{
+    BaseURL: "https://lakasir.test",
+})
+
+resp, err := apiClient.
+		UseHook(ExampleHook)
+```
--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"strings"
 	"time"
 )
+
+type Middleware func(req *http.Request, next http.HandlerFunc) http.HandlerFunc
+
+type Hook func(req *http.Request, resp *http.Response)
 
 type Opt struct {
 	BaseURL string
@@ -19,12 +24,24 @@ type Opt struct {
 }
 
 type Client struct {
-	httpClient *http.Client
-	baseURL    string
-	retries    int
-	headers    http.Header
+	httpClient  *http.Client
+	baseURL     string
+	retries     int
+	headers     http.Header
 	queryParams url.Values
-	output     interface{}
+	output      interface{}
+	middleware  []Middleware
+	hooks       []Hook
+}
+
+func (c *Client) UseMiddleware(middleware ...Middleware) *Client {
+	c.middleware = append(c.middleware, middleware...)
+	return c
+}
+
+func (c *Client) UseHook(hooks ...Hook) *Client {
+	c.hooks = append(c.hooks, hooks...)
+	return c
 }
 
 func New(opt Opt) *Client {
@@ -32,9 +49,9 @@ func New(opt Opt) *Client {
 		httpClient: &http.Client{
 			Timeout: opt.Timeout,
 		},
-		baseURL:    opt.BaseURL,
-		retries:    opt.Retries,
-		headers:    http.Header{},
+		baseURL:     opt.BaseURL,
+		retries:     opt.Retries,
+		headers:     http.Header{},
 		queryParams: url.Values{},
 	}
 }
@@ -82,100 +99,116 @@ func (c *Client) SetOutput(output interface{}) *Client {
 	return c
 }
 
-func (c *Client) Get(endpoint string) (*Response, error) {
-	req, err := http.NewRequest("GET", c.baseURL+endpoint, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.URL.RawQuery = c.queryParams.Encode()
-
-	for key, values := range c.headers {
-		for _, value := range values {
-			req.Header.Add(key, value)
-		}
-	}
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, _ := io.ReadAll(resp.Body)
-
-	var output interface{}
-	if c.output != nil {
-		output = c.output
-		err = json.Unmarshal(body, output)
+func (c *Client) doRequest(method, endpoint string, body interface{}) (*Response, error) {
+	var reqBody io.Reader
+	if body != nil {
+		jsonBody, err := json.Marshal(body)
 		if err != nil {
 			return nil, err
 		}
+		reqBody = bytes.NewBuffer(jsonBody)
 	}
 
+	req, err := http.NewRequest(method, c.baseURL+endpoint, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	if method == "GET" || method == "DELETE" {
+		req.URL.RawQuery = c.queryParams.Encode()
+	} else if method == "POST" || method == "PUT" || method == "PATCH" {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	c.addHeaders(req)
+
+	var request Request
+	var reqBodyBytes []byte
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		resp, err := c.httpClient.Do(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		// Apply hooks
+		for _, hook := range c.hooks {
+			hook(r, resp)
+		}
+
+		respBody, _ := io.ReadAll(resp.Body)
+
+		var output interface{}
+		if c.output != nil {
+			output = c.output
+			err = json.Unmarshal(respBody, output)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		if reqBody != nil {
+			reqBodyBytes, _ = io.ReadAll(reqBody)
+		}
+
+		request = Request{
+			Method:  method,
+			URL:     req.URL.String(),
+			Headers: req.Header,
+			Body:    reqBodyBytes,
+		}
+
+		w.Header().Set("StatusCode", fmt.Sprintf("%d", resp.StatusCode))
+		_, err = w.Write(respBody)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	for i := len(c.middleware) - 1; i >= 0; i-- {
+		handler = c.middleware[i](req, handler)
+	}
+
+	recorder := httptest.NewRecorder()
+	handler.ServeHTTP(recorder, req)
+
 	return &Response{
-		StatusCode: resp.StatusCode,
-		Body:       body,
-		Output:     output,
-		Request: &Request{
-			Method:     "GET",
-			URL:        req.URL.String(),
-			Headers:    req.Header,
-			Body:       nil,
-			QueryParams: c.queryParams,
-		},
+		StatusCode: recorder.Code,
+		Body:       recorder.Body.Bytes(),
+		Output:     c.output,
+		Request: &request,
 	}, nil
 }
 
+func (c *Client) Get(endpoint string) (*Response, error) {
+	return c.doRequest("GET", endpoint, nil)
+}
+
+func (c *Client) Delete(endpoint string) (*Response, error) {
+	return c.doRequest("DELETE", endpoint, nil)
+}
+
 func (c *Client) Post(endpoint string, body interface{}) (*Response, error) {
-	jsonBody, err := json.Marshal(body)
-	if err != nil {
-		return nil, err
-	}
+	return c.doRequest("POST", endpoint, body)
+}
 
-	req, err := http.NewRequest("POST", c.baseURL+endpoint, bytes.NewBuffer(jsonBody))
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", "application/json")
+func (c *Client) Put(endpoint string, body interface{}) (*Response, error) {
+	return c.doRequest("PUT", endpoint, body)
+}
 
-	// Add headers
+func (c *Client) Patch(endpoint string, body interface{}) (*Response, error) {
+	return c.doRequest("PATCH", endpoint, body)
+}
+
+func (c *Client) addHeaders(req *http.Request) {
 	for key, values := range c.headers {
 		for _, value := range values {
 			req.Header.Add(key, value)
 		}
 	}
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	respBody, _ := io.ReadAll(resp.Body)
-
-	// Unmarshal the response body into the output interface
-	var output interface{}
-	if c.output != nil {
-		output = c.output
-		err = json.Unmarshal(respBody, output)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return &Response{
-		StatusCode: resp.StatusCode,
-		Body:       respBody,
-		Output:     output,
-		Request: &Request{
-			Method:     "POST",
-			URL:        req.URL.String(),
-			Headers:    req.Header,
-			Body:       jsonBody,
-			QueryParams: c.queryParams,
-		},
-	}, nil
 }
 
 type Response struct {
@@ -186,10 +219,10 @@ type Response struct {
 }
 
 type Request struct {
-	Method     string
-	URL        string
-	Headers    http.Header
-	Body       []byte
+	Method      string
+	URL         string
+	Headers     http.Header
+	Body        []byte
 	QueryParams url.Values
 }
 
@@ -201,13 +234,11 @@ func (r *Request) GenerateCurlCommand() string {
 	curlCommand.WriteString(r.URL)
 	curlCommand.WriteString("\"")
 
-	// Add query parameters
 	if len(r.QueryParams) > 0 {
 		curlCommand.WriteString("?")
 		curlCommand.WriteString(r.QueryParams.Encode())
 	}
 
-	// Add headers
 	for key, values := range r.Headers {
 		for _, value := range values {
 			curlCommand.WriteString(" -H \"")
@@ -218,8 +249,7 @@ func (r *Request) GenerateCurlCommand() string {
 		}
 	}
 
-	// Add body for POST requests
-	if r.Method == "POST" && len(r.Body) > 0 {
+	if (r.Method == "POST" || r.Method == "PUT" || r.Method == "PATCH") && len(r.Body) > 0 {
 		curlCommand.WriteString(" -d '")
 		curlCommand.WriteString(string(r.Body))
 		curlCommand.WriteString("'")
